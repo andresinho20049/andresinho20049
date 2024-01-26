0e9c71721a0b3cd0fa59a066a333ecc2a3442249
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-01-26 14:07:23 -0300

    feat: start new page

---
+++ src/app/(content)/articles/page.tsx
@@ -0,0 +1,20 @@
+import { allPosts } from "contentlayer/generated"
+import { PreviewArticle } from '@/components/cards/preview-article'
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <div className="prose dark:prose-invert">
+      {allPosts.map((post) => (
+          <PreviewArticle
+            key={post._id}
+            title={post.title}
+            resume={post.description || "Summary not available"}
+            img_url={post.capa}
+            img_alt=""
+            slug={post.slug}
+          />
+      ))}
+    </div>
+  );
+}
