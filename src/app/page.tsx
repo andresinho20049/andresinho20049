a7d6c5fe49bb2402132b2718b2e3dacd3419bbbc
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-01-30 15:04:59 -0300

    h-screen to dvh responsive mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default function Home() {
 
   const username = `Andresinho20049`;
-  const summary = `Hi there! It's great to meet you. I'm André Carlos, known on the web as andresinho20049. 
+  const summary = `Hi there! I'm André Carlos, known on the web as andresinho20049. 
                   I'd love to take a moment to introduce myself and share a bit about my portfolio.`;
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen overflow-hidden bg-cover bg-center bg-art_andresinho_mobile_bg sm:bg-art_andresinho_bg">
+    <div className="flex flex-col items-center justify-center h-dvh transition-transform overflow-hidden bg-cover bg-center bg-art_andresinho_mobile_bg sm:bg-art_andresinho_bg">
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
       <h1
@@ -21,8 +21,8 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
-      <div className="my-16 mx-2 text-center animate-fade-in p-2 bg-gradient-to-r from-primary-700/30 via-yellow-300/15 to-purple-300/30 rounded-lg w-1/2 self-start sm:self-center">
-        <h2 className="text-lg text-white font-mono drop-shadow-md break-keep whitespace-pre-line">
+      <div className="my-16 mx-2 text-center animate-fade-in p-2 shadow-lg border border-white rounded-lg w-1/2 self-start sm:self-center">
+        <h2 className="text-md sm:text-lg text-white font-mono drop-shadow-md break-keep whitespace-pre-line">
           {summary}
         </h2>
       </div>
@@ -30,7 +30,7 @@ export default function Home() {
       <div className="w-48 animate-fade-in">
         <Link
           href={"/home"}
-          className="outline outline-pink-500 hover:outline-pink-300 dark:text-white dark:hover:text-gray-400 hover:text-gray-400 text-gray-900 text-lg font-medium px-4 py-2 rounded shadow"
+          className="outline outline-pink-500 hover:outline-pink-300 bg-gray-500 lg:bg-transparent text-white hover:text-gray-400 text-lg font-medium px-4 py-2 rounded shadow animate-pulse"
         >
           Get Started
         </Link>
