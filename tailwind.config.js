192fa765bf8e0a95479a14eb6729cd49afbb4e34
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-01-30 14:14:56 -0300

    feat animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const { url } = require('inspector');
-const colors = require('tailwindcss/colors');
+const { url } = require("inspector");
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -27,7 +27,7 @@ module.exports = {
       primary: colors.cyan,
       secondary: colors.pink,
       zinc: colors.zinc,
-      cyan: colors.cyan
+      cyan: colors.cyan,
     },
     screens: {
       sm: "640px",
@@ -67,6 +67,26 @@ module.exports = {
         title: "title 3s ease-out forwards",
         "fade-left": "fade-left 3s ease-in-out forwards",
         "fade-right": "fade-right 3s ease-in-out forwards",
+        typing: "typing 2s steps(20) normal forwards, blink .7s infinite",
+        transformOpacity: "transformOpacity 5s infinite alternate-reverse",
+        sizeOpacity:
+          "ping 3s infinite alternate-reverse, opacityEffect 8s infinite alternate-reverse",
+        bounceOpacityLeft:
+          "bounce 1s infinite, fade-left 8s infinite alternate-reverse",
+        bounceOpacityRight:
+          "bounce 1s infinite, fade-right 8s infinite alternate-reverse",
+        bounceOpacity:
+          "bounce 1s infinite, opacityEffect 8s infinite alternate",
+        pulseOpacity:
+          "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite, skewEffect 3s linear infinite alternate, opacityEffect 5s infinite alternate ",
+        fadeOutRight:
+          "fade-in 3s ease-in-out backwards,  fade-right 5s forwards 3 alternate",
+        fadeOutLeft:
+          "fade-in 3s ease-in-out backwards,  fade-left 5s forwards 3 alternate",
+        fadeOutSkew:
+          "fade-in 3s ease-in-out forwards, skewEffect 3s infinite alternate, opacityEffect 5s 10 forwards ",
+        skewEffect:
+          "skewEffect 2s cubic-bezier(0.4, 0, 0.6, 1) infinite, opacityEffect 5s infinite alternate-reverse ",
       },
       keyframes: {
         "fade-in": {
@@ -125,6 +145,63 @@ module.exports = {
           "100%": {
             "line-height": "100%",
             opacity: "100%",
+          },
+        },
+        typing: {
+          "0%": {
+            width: "0%",
+            opacity: "0%"
+          },
+          "10%": {
+            opacity: "100%"
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent",
+          },
+          // "100%": {
+          //   borderColor: "white",
+          // },
+        },
+        transformOpacity: {
+          "0%": {
+            transform: "translateX(-50%) translateY(0%) skewY(-6deg)",
+          },
+          "30%": {
+            transform: "translateX(30%)  skewY(10deg)",
+            opacity: "0%",
+          },
+          "75%": {
+            opacity: "0%",
+          },
+          "100%": {
+            transform: "translateY(50%) translateX(30%) skewY(6deg)",
+          },
+        },
+        opacityEffect: {
+          "0%": {
+            opacity: "0%",
+          },
+          "10%": {
+            opacity: "100%",
+          },
+          "20%": {
+            opacity: "0%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        skewEffect: {
+          "30%": {
+            transform: "skewY(-10deg)",
+          },
+          "75%": {
+            transform: "skewY(10deg)",
           },
         },
       },
