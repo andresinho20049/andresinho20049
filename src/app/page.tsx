74a8f289bcfc206130c1638ce3a3ed85f872d59c
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-02-02 22:01:32 -0300

    improve design and layout, componentize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
 
       <h1
         className="z-10 text-4xl md:text-5xl lg:text-8xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title 
-        font-semibold font-display  whitespace-nowrap bg-clip-text hover:bg-gradient-to-r from-white via-white to-primary-300"
+        font-semibold font-display  whitespace-nowrap bg-clip-text hover:bg-gradient-to-b from-white via-white to-primary-300"
       >
         {username}
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
-      <div className="my-16 mx-2 text-center animate-fade-in p-2 shadow-lg border border-white rounded-lg w-1/2 self-start sm:self-center">
+      <div className="my-16 mx-2 text-center animate-fade-in p-2 shadow-lg border border-white rounded-lg w-10/12 sm:w-8/12 md:w-6/12">
         <h2 className="text-md sm:text-lg text-white font-mono drop-shadow-md break-keep whitespace-pre-line">
           {summary}
         </h2>
--- src/components/button/button-component.tsx
+++ src/components/button/button-component.tsx
@@ -5,14 +5,16 @@ import { ReactNode } from "react";
 
 type IButtonComponentProps = ButtonProps & {
     children: ReactNode;
+    isLoading?: boolean;
 }
 
 export const ButtonComponent = ({
     children, 
+    isLoading,
     ...props
 }:IButtonComponentProps) => {
     return (
-        <Button {...props} outline color="blue">
+        <Button {...props} outline color="blue" disabled={isLoading} className={isLoading ? "animate-pulse" : ""}>
             {children}
         </Button>
     )
