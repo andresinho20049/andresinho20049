da9f8a58825e90ae0d553a7c3fd540c1631497c6
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-02-17 01:01:47 -0300

    adjust appearance at light theme

--- src/components/button/link-appearance-button.tsx
+++ src/components/button/link-appearance-button.tsx
@@ -14,7 +14,7 @@ export const LinkAppearanceButton = ({
 }: ILinkAppearanceButtonProps) => {
   return (
     <Link
-      className="flex items-center justify-center gap-1 text-white hover:text-gray-400 bg-blue-500 dark:bg-transparent text-md font-medium px-4 py-1 border-2 border-blue-200 border-y-blue-500 hover:border-y-blue-200 transition-colors rounded-md"
+      className="flex items-center justify-center gap-1 text-white hover:text-gray-400 bg-blue-700 hover:bg-blue-500 transition-colors duration-200 ease-in dark:bg-transparent text-md font-medium px-4 py-1 border-2 border-blue-200 border-y-blue-500 hover:border-y-blue-200 transition-colors rounded-md"
       href={href}
       target={isBlank ? "_blank" : ""}
       rel={isBlank ? "noopener noreferrer" : ""}
