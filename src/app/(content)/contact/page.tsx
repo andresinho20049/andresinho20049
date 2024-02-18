7a41ca15c745637160ba8359c55624f4dd703ed7
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-02-17 22:18:26 -0300

    Schema org, added on pages

--- src/app/(content)/contact/page.tsx
+++ src/app/(content)/contact/page.tsx
@@ -17,7 +17,7 @@ export default function Contact() {
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-32">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <h2 className="text-3xl font-bold text-blue-700">Contact Us</h2>
+            <h1 className="text-3xl font-bold text-blue-700">Contact Us</h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Feel free to contact us, we will get back to you as soon as
               possible.
--- src/components/structure/schema-org.tsx
+++ src/components/structure/schema-org.tsx
@@ -1,5 +1,5 @@
 import Script from "next/script";
-import type { Graph, Thing, WithContext } from "schema-dts";
+import type { Thing, WithContext } from "schema-dts";
 
 export function JsonLd<T extends Thing>(json: WithContext<T>) {
   return (
@@ -12,43 +12,3 @@ export function JsonLd<T extends Thing>(json: WithContext<T>) {
     />
   );
 }
-
-const date = new Date().toISOString().split('T')[0];
-
-export const GraphSchemaOrg: Graph = {
-  "@context": "https://schema.org",
-  "@graph": [
-    {
-      "@type": "Blog",
-      "@id": "https://andresinho20049.com.br/home",
-      name: "André Carlos",
-      description:
-        "Hello, I'm André Carlos, I'm a developer with a bachelor's degree in Computer Science and more than 5 years of experience, I present my portfolio.",
-      about: "Introduce me for you",
-      copyrightYear: "2024",
-      award: ["Developer", "Software engineer", "Full stack"],
-      dateCreated: "2024-01-26",
-      dateModified: date,
-      image: [
-        "https://andresinho20049.com.br/Logo.png",
-        "https://andresinho20049.com.br/avatar/Me-min.png",
-      ],
-      headline: "I'm André Carlos",
-    },
-    {
-      "@type": "ContactPage",
-      "@id": "https://andresinho20049.com.br/contact",
-      name: "Contact | Andresinho20049",
-      description:
-        "Feel free to contact us (andresinho20049 team), we will get back to you as soon as possible.",
-    },
-    {
-      "@type": "AboutPage",
-      "@id": "https://andresinho20049.com.br/about",
-      name: "About | Andresinho20049",
-      description:
-        "A responsive website demonstrating some of my skills linked to its delivery and development.",
-      mentions: ["Nextjs", "Tailwind", "Photoshop", "React", "Typescript"],
-    },
-  ],
-};
